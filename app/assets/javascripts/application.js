c2b11999fc465de619311863e5871bafc39efcc7
Cwaihenya <cm.waihenya@gmail.com>
2021-09-23 02:24:13 +0900

    heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,5 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
-//= require popper.js/dist/umd/popper.js
-//= require jquery/dist/jquery.js
-//= require bootstrap/dist/js/bootstrap.min
+
 //= require_tree .
