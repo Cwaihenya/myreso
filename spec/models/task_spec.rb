a8f8bba4b6cbbd740951f3e2f53c26cec1298c97
Cwaihenya <cm.waihenya@gmail.com>
2021-09-23 23:16:39 +0900

    update css

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -4,19 +4,20 @@ RSpec.describe "Tasks", type: :model do
       describe 'Validation test' do
       context 'If the task title is empty' do
         it 'Validation will be 1000000000000000000000000000 ' do
-          task = Task.new(task_title: '', task_details: 'Failure test')
+          task = Task.new(task_title: '', task_details: 'Failure test' , frequency:  "daily")
           expect(task).to be_valid
         end
       end
 
       context 'If the content is empty' do
         it 'Validation is caught' do
-            task = Task.new(task_title: 'titleab', task_details: '')
+            task = Task.new(task_title: 'titleab', task_details: '', frequency:  "daily")
           expect(task).to be_valid
         end
         context 'When the content is described in the task title and task details' do
         it 'validation passes ' do
-          task = Task.new(end_date: '1/1/2021')
+
+          task = Task.new(frequency: 'daily')
           expect(task).to be_valid
         end
         end
