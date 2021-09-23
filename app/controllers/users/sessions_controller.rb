11da783b9a07daeb59469998bf4de4378b8efdb5
Cwaihenya <cm.waihenya@gmail.com>
2021-09-23 18:02:19 +0900

    final guest log in

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -6,12 +6,12 @@ class Users::SessionsController < Devise::SessionsController
    def guest_sign_in
      user = User.guest
      sign_in user
-     redirect_to jobs_path, notice: 'You have logged in as a guest user.'
+     redirect_to blogs_path, notice: 'You have logged in as a guest user.'
    end
 
    def admin_guest_sign_in
      guest_user = User.admin_guest
      sign_in guest_user
-     redirect_to jobs_path, notice: 'You have logged in as an admin guest user.'
+     redirect_to blogs_path, notice: 'You have logged in as an admin guest user.'
    end
  end
